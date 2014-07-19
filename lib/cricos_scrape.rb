bf5dc5562b6e08601f2b1e76a655596b08d84151
Toàn Lê <ktoanlba@gmail.com>
2014-07-19 12:57:31 +0700

    Course Importer + Fixture

--- lib/cricos_scrape.rb
+++ lib/cricos_scrape.rb
@@ -4,4 +4,8 @@ require 'mechanize'
 require_relative 'cricos_scrape/address'
 require_relative 'cricos_scrape/institution'
 require_relative 'cricos_scrape/location'
+require_relative 'cricos_scrape/course'
+require_relative 'cricos_scrape/contact'
+
+require_relative 'cricos_scrape/course_importer'
 require_relative 'cricos_scrape/institution_importer'
---
+++ lib/cricos_scrape/contact.rb
@@ -0,0 +1,2 @@
+class Contact < Struct.new(:type_of_course, :name, :organisation, :postal_address, :telephone, :facsimile, :email)
+end
