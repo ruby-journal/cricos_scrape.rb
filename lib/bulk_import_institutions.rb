55ee661fb18249bc0aa9d003bc553732eabea1df
Toàn Lê <ktoanlba@gmail.com>
2014-08-02 22:49:34 +0700

    Clean BulkImportInstitutions #11

--- lib/bulk_import_institutions.rb
+++ lib/bulk_import_institutions.rb
@@ -1,42 +1,38 @@
 class BulkImportInstitutions
-  attr_reader :results_file, :id_min, :id_max, :id_file
+  attr_reader :output, :min_id, :max_id, :input
 
-  def initialize(results_file, id_min, id_max, id_file)
-    @id_min, @id_max, @id_file = [id_min, id_max, id_file]
+  def initialize(output, min_id, max_id, input=nil)
+    @range = input.nil? ? (min_id..max_id) : convert_ids_from_file_to_array(input)
     @institution_importer = CricosScrape::InstitutionImporter.new
-    @institutions_file = JsonFileStore.new(results_file['data'])
-    @institution_ids_file = JsonFileStore.new(results_file['ids'])
+    @institutions_file = JsonFileStore.new(output['data'])
+    @institution_ids_file = JsonFileStore.new(output['ids'])
   end
 
   def perform
-    id_range.each do |providerID|
+    @range.each do |providerID|
       puts "Success with ProviderID #{providerID}" if scrape_institution_and_save_to_file(providerID)
     end
   end
 
   private
   def scrape_institution_and_save_to_file(providerID)
-    @institution = @institution_importer.scrape_institution(providerID)
+    institution = @institution_importer.scrape_institution(providerID)
     
-    if institution_existed?
-      @institutions_file.save(@institution)
-      @institution_ids_file.save(providerID)
+    if institution
+      begin
+        @institutions_file.save(institution)
+        @institution_ids_file.save(providerID)
+      rescue => e
+        @institutions_file.rollback
+        @institution_ids_file.rollback
+        puts "An error occurred with ProviderID #{providerID}"
+      end
     else
       return false
     end
-  rescue => e
-    puts "An error occurred with ProviderID #{providerID}"
   end
 
-  def id_range
-    @id_file.nil? ? (@id_min..@id_max) : convert_ids_from_file_to_array
-  end
-
-  def convert_ids_from_file_to_array
-    File.read(@id_file).split(",").map { |s| s.to_i }
-  end
-
-  def institution_existed?
-    !!@institution
+  def convert_ids_from_file_to_array(input)
+    File.read(input).split(",").map { |s| s.to_i }
   end
 end
