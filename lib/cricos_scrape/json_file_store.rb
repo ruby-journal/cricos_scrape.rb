1f066cdd892333b63545dca97a45fca67b6e1cf5
Toàn Lê <ktoanlba@gmail.com>
2014-08-02 22:45:22 +0700

    Update Rollback JsonFileStore #13

--- lib/cricos_scrape/json_file_store.rb
+++ lib/cricos_scrape/json_file_store.rb
@@ -4,6 +4,7 @@ class JsonFileStore
   end
 
   def save(entity)
+    backup_old_file
     @entity = entity
     
     if empty_data_file?
@@ -11,13 +12,29 @@ class JsonFileStore
     else
       append_data_to_file
     end
+
+    remove_tmp_file
   end
 
   def empty_data_file?
     !File.exist?(file_path) || File.zero?(file_path)
   end
 
+  def rollback
+    FileUtils.rm_rf(file_path)
+    FileUtils.copy(file_path("tmp_#{@file}"), file_path) unless empty_data_file?
+    FileUtils.rm_rf(file_path("tmp_#{@file}"))
+  end
+
   private
+  def backup_old_file
+    FileUtils.copy(file_path, file_path("tmp_#{@file}")) unless empty_data_file?
+  end
+
+  def remove_tmp_file
+    FileUtils.rm_rf(file_path("tmp_#{@file}"))
+  end
+
   def save_data_to_new_file
     data_string = @entity.is_a?(Array) ? @entity.to_json : "[#{@entity.to_json}]"
     File.open(file_path, "a") { |file| file.write(data_string) }
@@ -32,7 +49,7 @@ class JsonFileStore
     end
   end
 
-  def file_path
-    File.expand_path("../../../data/#{@file}", __FILE__)
+  def file_path(file = @file)
+    File.expand_path("../../../data/#{file}", __FILE__)
   end
 end
