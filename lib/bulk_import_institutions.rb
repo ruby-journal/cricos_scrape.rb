71e94e1eafe4a571d267e08dd844f93d30043522
Toàn Lê <ktoanlba@gmail.com>
2014-08-02 23:16:12 +0700

    Clean up BulkImportInstitutions #11

--- lib/bulk_import_institutions.rb
+++ lib/bulk_import_institutions.rb
@@ -2,8 +2,10 @@ class BulkImportInstitutions
   attr_reader :output, :min_id, :max_id, :input
 
   def initialize(output, min_id, max_id, input=nil)
-    @range = input.nil? ? (min_id..max_id) : convert_ids_from_file_to_array(input)
-    @institution_importer = CricosScrape::InstitutionImporter.new
+    @range = input ? convert_ids_from_file_to_array(input) : (min_id..max_id)
+    return error_notice if @range.empty?
+
+    @importer = CricosScrape::InstitutionImporter.new
     @institutions_file = JsonFileStore.new(output['data'])
     @institution_ids_file = JsonFileStore.new(output['ids'])
   end
@@ -16,23 +18,31 @@ class BulkImportInstitutions
 
   private
   def scrape_institution_and_save_to_file(providerID)
-    institution = @institution_importer.scrape_institution(providerID)
+    institution = @importer.scrape_institution(providerID)
     
     if institution
-      begin
-        @institutions_file.save(institution)
-        @institution_ids_file.save(providerID)
-      rescue => e
-        @institutions_file.rollback
-        @institution_ids_file.rollback
-        puts "An error occurred with ProviderID #{providerID}"
-      end
+      save_institution_data_to_file
     else
-      return false
+      error_notice
     end
+  end
+
+  def save_institution_data_to_file
+    @institutions_file.save(institution)
+    @institution_ids_file.save(providerID)
+  rescue => e
+    @institutions_file.rollback
+    @institution_ids_file.rollback
+    error_notice
+  end
+
+  def error_notice
+    puts "An error occurred with ProviderID #{providerID}"
   end
 
   def convert_ids_from_file_to_array(input)
     File.read(input).split(",").map { |s| s.to_i }
+  rescue => e
+    puts "Wrong input format file"
   end
 end
