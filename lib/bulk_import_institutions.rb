d70d43e4202b908f5093ca751027861fc868428f
Toàn Lê <ktoanlba@gmail.com>
2014-08-03 13:03:32 +0700

    BulkImportInstitutions

---
+++ lib/bulk_import_institutions.rb
@@ -0,0 +1,37 @@
+class BulkImportInstitutions
+  attr_reader :output, :min_id, :max_id, :input
+
+  def initialize(output, min_id, max_id, input=nil)
+    @range = input ? convert_ids_from_file_to_array(input) : (min_id..max_id).to_a
+    return 'Invalid Range ID' if @range.empty?
+
+    @importer = CricosScrape::InstitutionImporter.new
+    @institutions_file = JsonFileStore.new(output['data'])
+    @institution_ids_file = JsonFileStore.new(output['ids'])
+  end
+
+  def perform
+    @range.each do |providerID|
+      puts "Success with ProviderID #{providerID}" if scrape_institution_and_save_to_file(providerID)
+    end
+  end
+
+  private
+  def scrape_institution_and_save_to_file(providerID)
+    institution = @importer.scrape_institution(providerID)
+    institution ? save_institution_data_to_file(providerID, institution) : (puts "The Provider ID #{providerID} entered is invalid - please try another.")
+  end
+
+  def save_institution_data_to_file(providerID, institution)
+    @institution_ids_file.save(providerID)
+    @institutions_file.save(institution)
+  rescue => e
+    @institutions_file.rollback
+    @institution_ids_file.rollback
+    puts "Error writing to files with ProviderID #{providerID}"
+  end
+
+  def convert_ids_from_file_to_array(input)
+    File.read(input).split(",").map { |s| s.to_i } rescue []
+  end
+end
