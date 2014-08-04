97b89ec336467a07f0cf6ff11907427e4cb33752
Toàn Lê <ktoanlba@gmail.com>
2014-08-04 12:44:38 +0700

    BulkImportContacts

---
+++ lib/bulk_import_contacts.rb
@@ -0,0 +1,30 @@
+class BulkImportContacts
+  attr_reader :results_file
+
+  def initialize(results_file, overwrite = false)
+    @overwrite = overwrite
+    @contacts_file = JsonFileStore.new(results_file)
+    @contact_importer = CricosScrape::ContactImporter.new
+  end
+
+  def perform
+    if should_create_new_data_contacts_file?
+      scrape_contact_and_save_to_file
+      puts "Success to get Contacts"
+    else
+      puts "Contacts have been taken"
+    end
+  end
+
+  private
+  def scrape_contact_and_save_to_file
+    contacts = @contact_importer.scrape_contact
+    @contacts_file.save(contacts)
+  rescue => e
+    puts "An error occurred when scrape contacts"
+  end
+
+  def should_create_new_data_contacts_file?
+    @contacts_file.file_empty? || @overwrite
+  end
+end
